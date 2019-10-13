0275a7459a891048fb1a4d51fbce575fe15d8df9
William <wecalderonc@unal.edu.co>
2019-10-13 02:10:14 -0500

    more setup db config

---
+++ platziverse-db/setup.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')
+
+const prompt = inquirer.createPromptModule()
+
+async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy db, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+  
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    setup: true,
+    logging: s => debug(s)
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
