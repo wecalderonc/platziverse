b9dfcdb618dbdbb012eaa05924e6ba3bf0609890
William <wecalderonc@unal.edu.co>
2019-12-01 22:43:37 -0500

    add findbyid testing

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -4,8 +4,13 @@ const test = require('ava')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
+const agentFixtures = require('./fixtures/agent')
+
+let single = Object.assign({}, agentFixtures.single)
+let id = 1
 let db = null
 let sandbox = null
+
 
 const config = {
   logging: function () {}
@@ -22,6 +27,11 @@ test.beforeEach(async () => {
   AgentStub = {
     hasMany: sandbox.spy()
   }
+
+  //Nidek findById Stub
+
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
 
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
@@ -44,4 +54,14 @@ test.serial('Setup', t => {
   t.true(AgentStub.hasMany.calledWith(MetricStub), ' Argument should be the Model')
   t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
   t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the Model')
+})
+
+test.serial('Agent#FindById', async t => {
+  let agent = await db.Agent.findById(id)
+
+  t.true(AgentStub.findById.called, 'findById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with args')
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
