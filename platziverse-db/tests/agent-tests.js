eeb530ffe44c05fb77d8134999bbbef52a13e086
William <wecalderonc@unal.edu.co>
2019-12-01 21:54:47 -0500

    add first test stubs o mocks

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -1,17 +1,47 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 let db = null
+let sandbox = null
+
 const config = {
   logging: function () {}
 }
 
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub = null
+
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.resetHistory()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), ' Argument should be the Model')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the Model')
 })
